ce61d64226b41693693813cf756fc448a79b0a30
darkwlf <drk.wlf9@gmail.com>
2020-07-10 17:57:01 +0430

    Add models

--- music/models.py
+++ music/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+
+    artist = models.CharField(max_length=25)
+
+    album_title = models.CharField(max_length=500)
+
+    genre = models.CharField(max_length=100)
+
+    album_logo = models.CharField(max_length=900)
+
+    def __str__(self):
+
+        return self.album_title + ' - ' + self.artist
+
+class Song(models.Model):
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    file_type = models.CharField(max_length=900, default='mp3')
+
+    song = models.CharField(max_length=250, default='mp3')
+
+    addr = models.CharField(max_length=500, default='https://irsv.upmusics.com/99/Shahab%20Mozaffari%20-%20Rade%20Pa%20(320).mp3')
+
+    def __str__(self):
+
+        return 'Name: ' + str(self.song) + '   -    ' + str('Album : ') + str(self.album)
--- music/views.py
+++ music/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    HttpResponse,
+    HttpResponseRedirect,
+    Http404,
+)
 
-# Create your views here.
+from .models import (
+
+    Album,
+
+    Song,
+
+)
+
+import time
+
+def music(request):
+    context = {
+        'album' : Album.objects.all()
+    }
+    return render(request, 'templates/music.html', context)
+
+def detail(request, album_id):
+    #txt = Album.objects.filter(id=album_id)
+    try:
+        context = {
+        'album' : Album.objects.all()[album_id-1],
+        'id' : album_id,
+        }
+        return render(request, 'templates/album.html', context)
+    except IndexError:
+        raise Http404
+
+def main(request):
+
+    return render(request, 'templates/index.html')
